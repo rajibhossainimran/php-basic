96f90e2fb37914bae669a9a55c7c7d21eba2d506
rajibhossainimran <rajibhossainimran@gmail.com>
2024-11-12 10:00:12 +0600

    try print and echo

---
+++ variable.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "hello world";
+echo "<br>";
+print "used print";
+echo "<br>";
+printf('this is printf');
+echo "<br>";
+sprintf('this is sprintf');
+$num= '44';
+$num2= '22';
+$sum = $num+$num2;
+#var_dump($num);
+echo($sum);
+
+?>
