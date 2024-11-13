d0f358e097d55e877b4038435292f2816a5d892d
rajibhossainimran <rajibhossainimran@gmail.com>
2024-11-13 11:51:51 +0600

    server method

--- grade.php
+++ grade.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$mark = 80;
+$mark = 87;
 if($mark>=80){
     echo "Your mark $mark .You got A+";
     echo '<br>';
@@ -16,4 +16,14 @@ if($mark>=80){
     echo '<br>';
 }
 
+
+echo $_SERVER['SERVER_PORT'];
+echo '<br>';
+echo $_SERVER['SERVER_ADDR'];
+echo '<br>';
+echo $_SERVER['SCRIPT_NAME'];
+echo '<br>';
+echo $_SERVER['REQUEST_METHOD'];
+echo '<br>';
+echo $_SERVER['PHP_SELF'];
 ?>
