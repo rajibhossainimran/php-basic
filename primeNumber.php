0480086e4981cffd042d9d71ef33709d7b59f53e
rajibhossainimran <rajibhossainimran@gmail.com>
2024-11-13 13:20:14 +0600

    string methods

--- primeNumber.php
+++ primeNumber.php
@@ -1,5 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check Prime number</title>
+</head>
+<body>
+<form method="POST" action="">
+    <label for="num1">Number 1:</label>
+    <input type="number" id="num1" name="num1" required><br><br>
+    <input type="submit" value="check prime">
+</form>
+
 <?php
-$number = 4978937598374957349857;
+
+$number = $_POST['num1'];
+// $number = 4978937598374957349857;
 $count = 0;
     if ($number <= 1) {
         echo "$number not prime not composit";
@@ -18,3 +34,9 @@ if($count==0){
 }
 
 ?>
+
+</body>
+</html>
+
+
+
---
+++ strFunciton.php
@@ -0,0 +1,19 @@
+<?php
+$text ='my name is rajib';
+$len = strlen($text);
+echo "$text length is $len";
+
+
+echo '<br>';
+
+// conver lower case 
+echo strtolower('My Name Is Rajib');
+echo '<br>';
+
+// conver to upper case 
+echo strtoupper('My Name Is Rajib');
+echo '<br>';
+// substring 
+echo substr('Banglasesh',5);
+
+?>
