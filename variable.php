3307803c9c44b120f6053978e49ba6a94499b9e7
rajibhossainimran <rajibhossainimran@gmail.com>
2024-11-13 10:17:09 +0600

    try concatinate and slice

--- variable.php
+++ variable.php
@@ -12,5 +12,18 @@ $num2= '22';
 $sum = $num+$num2;
 #var_dump($num);
 echo($sum);
+echo "<br>";
+
+
+
+// concatinate string 
+$str = 'Rajib';
+$str2 ='Hossain';
+echo $str.' '.$str2;
+echo "<br>";
+
+// slice text using substr() method 
+$text = 'my name is md';
+echo substr($text,4,5);
 
 ?>
