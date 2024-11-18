e1219f711cdbe47fbaad3605d254d04341485597
rajibhossainimran <rajibhossainimran@gmail.com>
2024-11-18 10:55:31 +0600

    dynamic class added

--- oop/class.php
+++ oop/class.php
@@ -23,4 +23,31 @@ echo "<br><br>";
 // call the function inside the class 
 echo $my_car ->info();
 
+// Create a dynamic class
+class Bike {
+    public $name;
+    public $model;
+    public $color;
+
+    // Method to set the name and color
+    function set_name($fname, $fcolor) {
+        $this->name = $fname;
+        $this->color = $fcolor;
+    }
+
+    // Method to get the name and color (return the values)
+    function get_name() {
+        return "Name: " . $this->name . ", Color: " . $this->color;
+    }
+}
+
+// Create an object of the Bike class
+$my_bike = new Bike();
+
+// Set the name and color of the bike
+$my_bike->set_name("Bicycle", "Red");
+
+// Get the name and color of the bike and display it
+echo $my_bike->get_name();
+
 ?>
