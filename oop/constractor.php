738780c7e614ff5a73203e14f36fcd1371aa4220
rajibhossainimran <rajibhossainimran@gmail.com>
2024-11-19 12:27:59 +0600

    instanceof and method_exists check class and function

--- oop/constractor.php
+++ oop/constractor.php
@@ -5,7 +5,7 @@ class Car {
     public $model="01";
 
     function __destruct(){
-        echo "bye bye";
+        // echo "bye bye";
     }
 
     function info(){
@@ -29,13 +29,25 @@ class Bike extends Car{
         echo "this is chile method <br>";
     }
     function __construct(){
-        
+
     }
 }
 
 $bikeObj = new Bike();
-$bikeObj->details();
-echo $bikeObj->color;
+// $bikeObj->details();
+// echo $bikeObj->color;
 // echo $bikeObj->name;
 
+
+if ($bikeObj instanceof Bike) {
+    echo "This is a Car object.";  // Output: This is a Car object.
+} else {
+    echo "This is not a Car object.";
+}
+
+echo"<br>";
+// check the object have or not in the class outpur true or false
+var_dump($bikeObj instanceof Student);
+echo "<br>";
+var_dump(method_exists($bikeObj,"details"))
 ?>
--- oop/objWithoutNew.php
+++ oop/objWithoutNew.php
@@ -3,10 +3,18 @@
 class Student{
     public static $name="rajib hossain <br>";
     public static $roll="1285228 <br>";
+    public static function info(){
+        echo self::$name; 
+        echo self::$roll;
+    }
 }
 
-echo Student::$name;
-echo Student::$roll;
+// access Student class property 
 
+// echo Student::$name;
+// echo Student::$roll;
+
+// call the function 
+Student::info();
 
 ?>
