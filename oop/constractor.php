8d57fae7a9b258b6f6639fdb3aa0f14e79e042a9
rajibhossainimran <rajibhossainimran@gmail.com>
2024-11-19 11:15:13 +0600

    object without new keyword and access scope regulation operator

--- oop/constractor.php
+++ oop/constractor.php
@@ -1,26 +1,41 @@
 <?php
 
 class Car {
-public $name;
-public $model;
+    public $name;
+    public $model="01";
 
-function __destruct(){
-    echo "bye bye";
+    function __destruct(){
+        echo "bye bye";
+    }
+
+    function info(){
+        echo 'THIS IS NORMAL FUNCTION <br>';
+    }
+
+    function __construct($num){
+        $this->name = $num;
+        // echo "THIS IS A FUNCTION <br>";
+    }
+
+
 }
 
-function info(){
-    echo 'THIS IS NORMAL FUNCTION <br>';
+// $newObject = new Car('honddddddda');
+// echo $newObject->name;
+
+class Bike extends Car{
+    public $color ="RED";
+    function details(){
+        echo "this is chile method <br>";
+    }
+    function __construct(){
+        
+    }
 }
 
-function __construct(){
-    echo "THIS IS A FUNCTION <br>";
-}
-
-
-}
-
-$newObject = new Car();
-echo $newObject->info();
-
+$bikeObj = new Bike();
+$bikeObj->details();
+echo $bikeObj->color;
+// echo $bikeObj->name;
 
 ?>
---
+++ oop/objWithoutNew.php
@@ -0,0 +1,12 @@
+<?php
+
+class Student{
+    public static $name="rajib hossain <br>";
+    public static $roll="1285228 <br>";
+}
+
+echo Student::$name;
+echo Student::$roll;
+
+
+?>
