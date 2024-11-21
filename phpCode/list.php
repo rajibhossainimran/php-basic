52bfa8c230e459c067d82b5a57594b914e7b9b28
rajibhossainimran <rajibhossainimran@gmail.com>
2024-11-21 11:09:10 +0600

    list method added

---
+++ phpCode/list.php
@@ -0,0 +1,12 @@
+<?php
+
+$str = "my name is rajb";
+list($a,$b,$c,$d)=explode(" ",$str);
+    echo $a;
+    echo "<br>";
+    echo $b;
+    echo "<br>";
+    echo $c;
+    echo "<br>";
+    echo $d;
+?>
