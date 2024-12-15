fab64cd7b2e29b70357096936dad0549815d56d5
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-15 10:45:58 +0600

    file upload added

---
+++ evidance-php/fileUpload.php
@@ -0,0 +1,53 @@
+<?php 
+
+if (isset($_POST["upload"])) {
+    if (isset($_FILES["myFile"])) {
+        $imageFile = $_FILES["myFile"];
+        // var_dump($imageFile);
+    } else {
+        echo "No file uploaded.";
+    }
+
+    $fileName = $_FILES["myFile"]["name"];
+    $temp = $_FILES["myFile"]["tmp_name"];
+    $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    $imgLocaton = "uploads/";
+    $file_size = $_FILES["myFile"]["size"];
+    $kb = $file_size / 1024;
+
+    if (($kb > 100) || !in_array($fileType, ["jpg", "png", "jpeg", "gif"])) {
+        if ($kb > 100) {
+            $msg1 = "Image is too large. Your image must be a maximum of 100 KB.";
+        }
+        if (!in_array($fileType, ["jpg", "png", "jpeg", "gif"])) {
+            $msg2 = "Also Sorry, only jpg, png, jpeg, or gif formats are allowed!";
+        }
+    } else {
+        move_uploaded_file($temp, $imgLocaton . $fileName);
+        echo "Successfully Uploaded!";
+    }
+
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+</head>
+<body>
+    <section>
+        <h1>file upload dashboard</h1>
+        <form action="" method="post" enctype="multipart/form-data">
+            <label for="file">Uplaod file</label><br><br>
+            <input type="file" name="myFile"><br><br>
+            <button type="submit" name="upload">Uplaod</button>
+        </form>
+    </section>
+</body>
+</html>
---
+++ evidance-php/login.php
@@ -0,0 +1,54 @@
+<?php 
+
+if(isset($_POST["btnLogin"])){
+    $mail = $_POST["email"];
+    $password = $_POST['password'];
+
+    // get file email and password form storpass.txt file 
+    $file = file('storePass.txt');
+    foreach($file as $line){
+        list($email,$pass) = explode(',',$line);
+        if(!preg_match("/^[a-zA-Z0-9]{4,}[@][a-zA-Z]{4,6}[.][a-zA-Z]{2,3}$/",$email)){
+		
+            $messageEmail = "Please enter a valid email.";
+            
+        }else{
+            if(trim($email) == "$mail" && trim($pass) == "$password"){
+                $_SESSION["my_session"] = $email;
+                header("location:fileUpload.php");
+        }else{
+            $logERR = "Email or Password is incorrect";
+        }
+    }
+}
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <section>
+        <form action="" method="post">
+            <label for="email">Email</label><br><br>
+            <input type="text" name="email" placeholder="rajib@gmail.com"><br><br>
+            <?php 
+            echo isset($messageEmail) ? $messageEmail : '';
+            ?>
+
+            <label for="password">Password</label>
+            <input type="text" name="password" id="" placeholder="1234"><br><br>
+
+            <button type="submit" name="btnLogin">LOGIN</button>
+            <?php 
+            echo isset($logERR) ? $logERR : '';
+            ?>
+        </form>
+    </section>
+</body>
+</html>
